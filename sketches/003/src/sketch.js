8d11b5627b2e2c256b8fb8c87c4772a0f51cf420
Travis Shears <btbtravis@gmail.com>
2018-05-11 11:30:09 +0000

    Create moving iso platforms to build the rest of the peice off

--- sketches/003/src/sketch.js
+++ sketches/003/src/sketch.js
@@ -12,22 +12,34 @@ import "babel-polyfill";
 paper.install(window);
 const canvas = document.querySelector('canvas');
 
+// colors
+// http://paletton.com/#uid=31b1f0kMoI2nuMguhM2MFsyN-mb
+let colors = {
+  0: '#FFBF00',
+  1: '#28E600',
+  2: '#E50070',
+  3: '#FFFFFF',
+  bg0: '#E3AA00',
+  bg1: '#21BE00',
+  bg2: '#BC005C',
+  bg3: '#FFFFFF'
+};
+
+for (var key in colors) {
+  let colorHsl = cconvert.hex.hsl(colors[key]);
+  colors[colors[key]] = {
+    full: colors[key],
+    light: '#' + cconvert.hsl.hex(colorHsl[0], colorHsl[1], colorHsl[2] != 100 ? colorHsl[2] + 5 : 100),
+    dark: '#' + cconvert.hsl.hex(colorHsl[0], colorHsl[1], colorHsl[2] != 0 ? colorHsl[2] - 5 : 0)
+  }
+}
+
 window.onload = function() {
   paper.setup(canvas);
 
-  // colors
-  // http://paletton.com/#uid=31b1f0kMoI2nuMguhM2MFsyN-mb
-  let colors = {
-    0: '#FFBF00',
-    1: '#28E600',
-    2: '#E50070',
-    3: '#FFFFFF',
-    bg0: '#E3AA00',
-    bg1: '#21BE00',
-    bg2: '#BC005C',
-    bg3: '#FFFFFF'
-  };
 
+
+  console.log('colors: ', colors);
 
   //let ogPt = new Point(0, canvas.height);
   //let ogPt = new Point(canvas.width / 2, canvas.height / 2);
@@ -40,22 +52,58 @@ window.onload = function() {
 
 
   let basePath = new Path([a, b, c, d, a]);
-  basePath.strokeWeight = 2;
-  basePath.strokeColor = 'black';
+  //basePath.strokeWeight = 2;
+  //basePath.strokeColor = 'black';
   basePath.scale(100, a);
   basePath.translate(ogPt);
   console.log('basePath: ', basePath);
-  let recs = [];
-  for (var i = 0, rowCount = 10; i < rowCount; i++) {
-    for (var j = 0, colCount = 100; j < colCount; j++) {
-      let rec = basePath.clone();
-      let bgRandomVal = Math.floor(Math.random() * Math.floor(3));
-      rec.fillColor = colors['bg' + bgRandomVal];
-      rec.translate(new Point(0, ogPt.y - (basePath.bounds.height * j)));
-      rec.translate(new Point((basePath.segments[0].point.x - basePath.segments[1].point.x) * i * -1, (basePath.segments[0].point.y - basePath.segments[1].point.y) * i));
-      recs.push(rec);
-    }
+  let colRecs = [];
+  for (var j = 0, colCount = 100; j < colCount; j++) {
+    let colRec = basePath.clone();
+    colRec.translate(new Point(0, ogPt.y - (basePath.bounds.height * j)));
+    colRecs.push(colRec);
   }
+  colRecs.reverse();
+  let recs = colRecs.map((colRec, i) => {
+    let row = [];
+    for (var j = 0, rowCount = 10; j < rowCount; j++) {
+      let rowRec = colRec.clone();
+      //rowRec.translate(new Point(0, ogPt.y - (basePath.bounds.height * j)));
+      rowRec.translate(new Point((basePath.segments[0].point.x - basePath.segments[1].point.x) * j * -1, (basePath.segments[0].point.y - basePath.segments[1].point.y) * j));
+      row.push(rowRec);
+      //let idTxt = new PointText({
+        //point: rowRec.bounds.center,
+        //content: `${i}, ${j}`,
+        //fillColor: 'black',
+        //fontSize: 12
+      //});
+    }
+    //let idTxt = new PointText({
+      //point: colRec.bounds.center,
+      //content: i,
+      //fillColor: 'black',
+      //fontSize: 12
+    //});
+    return row;
+  });
+
+  recs = recs.reduce((carry, row) => {
+    return carry.concat(row);
+  }, []);
+
+
+  recs = recs.map(rec => {
+    let color = colors[Math.floor(Math.random() * Math.floor(3))];
+    return isoPlatform (rec, color);
+  });
+  //recs.forEach((rec, i) => {
+    //let idTxt = new PointText({
+      //point: rec.bounds.center,
+      //content: i,
+      //fillColor: 'black',
+      //fontSize: 12
+    //});
+  //});
 
   //let offset = 0;
   //document.addEventListener('keydown', (e) => {
@@ -72,9 +120,8 @@ window.onload = function() {
   paper.view.onFrame = (e) => {
     //if(e.count % 60  === 0) {
     //project.clear();
-    //
-    let speed = 5;
 
+    let speed = 5;
     if(e.count % 2 === 0) {
       recs.forEach(rec => {
         rec.translate(new Point(0, speed));
@@ -85,3 +132,38 @@ window.onload = function() {
   // Draw the view now:
   paper.view.draw();
 }
+
+
+function isoPlatform (rec, color) {
+  let a = rec.segments[0].point;
+  let b = rec.segments[1].point;
+  let c = rec.segments[2].point;
+  let d = rec.segments[3].point;
+  let gapX = Math.abs((d.x - b.x) / 20);
+  let gapY = Math.abs((a.y - c.y) / 20);
+  let ap = new Point(a.x, a.y - gapY);
+  let bp = new Point(b.x - gapX, b.y);
+  let cp = new Point(c.x, c.y + gapY);
+  let dp = new Point(d.x + gapX, d.y);
+
+  let platfromTop = new Path([ap, bp, cp, dp, ap]);
+  platfromTop.translate(new Point(0, gapY * -2.5));
+  //platfromTop.strokeWeight = 2;
+  //platfromTop.strokeColor = 'black';
+  platfromTop.fillColor = colors[color].light;
+
+  let platfromRight = new Path([a, platfromTop.segments[0].point, platfromTop.segments[1].point, b, a]);
+  //platfromRight.translate(new Point(0, gapY * -2.5));
+  //platfromRight.strokeWeight = 2;
+  //platfromRight.strokeColor = 'black';
+  platfromRight.fillColor = colors[color].full;
+
+  let platfromLeft = new Path([a, platfromTop.segments[0].point, platfromTop.segments[3].point, d, a]);
+  //platfromRight.translate(new Point(0, gapY * -2.5));
+  //platfromLeft.strokeWeight = 2;
+  //platfromLeft.strokeColor = 'black';
+  platfromLeft.fillColor = colors[color].dark;
+
+  return new Group([platfromTop, platfromRight, platfromLeft]);
+}
+
